66214e9f6db6b7ee6e00a4b90716eeb736437523
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-05-31 21:59:24 +0530

    refactor added pointers in go

--- main.go
+++ main.go
@@ -18,9 +18,25 @@ func main() {
 	const name1 string = "sadiq"
 	name2 := "Mohammad"
 	fmt.Println(name, name1, name2) */
+	/* var x int = 5
+	var p *int = &x
+	fmt.Println(p)
+	fmt.Println(*p) */
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Empty todo cannot be created"})
+		}
+		todo.ID=len(todos)+1
+		todos=append(todos)+1
+		return c.Status(201).JSON(todo)
 	})
 	log.Fatal(app.Listen(":3000"))
 }
