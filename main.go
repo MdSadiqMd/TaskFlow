476213c40e906ba747b2d2be50eb900112a4fa1e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-05-31 19:23:11 +0530

    go init

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* fmt.Println("Hello World")
+	var name string = "Md"
+	const name1 string = "sadiq"
+	name2 := "Mohammad"
+	fmt.Println(name, name1, name2) */
+	
+}
