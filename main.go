0d109fcf37d8c69cc9e57c9c9c5d15dde8690d5a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-05-31 19:38:33 +0530

    route with catch

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	/* fmt.Println("Hello World")
-	var name string = "Md"
+	fmt.Println("Hello World")
+	/* var name string = "Md"
 	const name1 string = "sadiq"
 	name2 := "Mohammad"
 	fmt.Println(name, name1, name2) */
-	
+	app := fiber.New()
+	log.Fatal(app.Listen(":3000"))
 }
