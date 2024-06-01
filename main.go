162c98b6b99cf7fb83112ed4c3a74d324d5e0057
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-01 19:09:06 +0530

    refactor added mongodb connection

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	/* "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive" */
 	"go.mongodb.org/mongo-driver/mongo"
-	/* "go.mongodb.org/mongo-driver/mongo/options" */
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"os"
 )
@@ -22,11 +23,6 @@ type Todo struct {
 
 func main() {
 	fmt.Println("Hello World")
-	err := godotenv.Load("env")
-	if err != nil {
-		log.Fatal("Error in loading .env File")
-	}
-	PORT := os.Getenv("PORT")
 	/* var name string = "Md"
 	const name1 string = "sadiq"
 	name2 := "Mohammad"
@@ -35,9 +31,27 @@ func main() {
 	var p *int = &x
 	fmt.Println(p)
 	fmt.Println(*p) */
+	err := godotenv.Load("env")
+	if err != nil {
+		log.Fatal("Error in loading .env File")
+	}
+	PORT := os.Getenv("PORT")
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	collection = client.Database("Tasks").Collection("todos")
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
 	})
+	app.Get("/api/todos", getTodos)
 	log.Fatal(app.Listen(":" + PORT))
 }
