1e8fceebbbc432b4fb0f5ed4861816370d9b0e76
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-01 20:30:27 +0530

    refactor added models

---
+++ routes/CreateTodo.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	/* "context"
+	"github.com/MdSadiqMd/TaskFlow/db" */
+	"github.com/gofiber/fiber/v2"
+	/* "go.mongodb.org/mongo-driver/bson"
+	"log" */
+)
+
+func createTodo(c *fiber.Ctx) error {
+
+} 
--- routes/getTodos.go
+++ routes/getTodos.go
@@ -3,19 +3,14 @@ package routes
 import (
 	"context"
 	"github.com/MdSadiqMd/TaskFlow/db"
+	"github.com/MdSadiqMd/TaskFlow/models"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"log"
 )
 
-type Todo struct {
-	ID        int    `json:"id" bson:"_id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body"`
-}
-
 func GetTodos(c *fiber.Ctx) error {
-	var todos []Todo
+	var todos []models.Todo
 
 	cursor, err := db.Collection.Find(context.Background(), bson.M{})
 	if err != nil {
@@ -25,7 +20,7 @@ func GetTodos(c *fiber.Ctx) error {
 	defer cursor.Close(context.Background())
 
 	for cursor.Next(context.Background()) {
-		var todo Todo
+		var todo models.Todo
 		if err := cursor.Decode(&todo); err != nil {
 			log.Fatal(err)
 			return c.Status(500).SendString("Failed to decode todo")
