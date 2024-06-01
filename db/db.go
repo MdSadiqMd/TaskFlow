e7dcb2648706638b29fb801c71af105f2d18ad7c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-01 20:06:04 +0530

    refactor added db connection

--- db/db.go
+++ db/db.go
@@ -1,46 +1,46 @@
 package db
 
 import (
-    "context"
-    "fmt"
-    "github.com/gofiber/fiber/v2"
-    "github.com/joho/godotenv"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "log"
-    "os"
+	"context"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
 )
 
 var collection *mongo.Collection
 
 func ConnectDB(c *fiber.Ctx) error {
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatal("Error loading .env file")
-        return c.Status(fiber.StatusInternalServerError).SendString("Error loading .env file")
-    }
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return c.Status(fiber.StatusInternalServerError).SendString("Error loading .env file")
+	}
 
-    MONGODB_URI := os.Getenv("MONGODB_URI")
-    if MONGODB_URI == "" {
-        log.Fatal("MONGODB_URI is not set in .env file")
-        return c.Status(fiber.StatusInternalServerError).SendString("MONGODB_URI is not set in .env file")
-    }
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	if MONGODB_URI == "" {
+		log.Fatal("MONGODB_URI is not set in .env file")
+		return c.Status(fiber.StatusInternalServerError).SendString("MONGODB_URI is not set in .env file")
+	}
 
-    clientOptions := options.Client().ApplyURI(MONGODB_URI)
-    client, err := mongo.Connect(context.Background(), clientOptions)
-    if err != nil {
-        log.Fatal(err)
-        return c.Status(fiber.StatusInternalServerError).SendString("Failed to connect to MongoDB")
-    }
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Failed to connect to MongoDB")
+	}
 
-    err = client.Ping(context.Background(), nil)
-    if err != nil {
-        log.Fatal(err)
-        return c.Status(fiber.StatusInternalServerError).SendString("Failed to ping MongoDB")
-    }
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Failed to ping MongoDB")
+	}
 
-    fmt.Println("Connected to MongoDB")
-    collection = client.Database("Tasks").Collection("todos")
+	fmt.Println("Connected to MongoDB")
+	collection = client.Database("Tasks").Collection("todos")
 
-    return c.Status(fiber.StatusOK).SendString("Connected to MongoDB")
+	return c.Status(fiber.StatusOK).SendString("Connected to MongoDB")
 }
