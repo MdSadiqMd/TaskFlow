95e6356cc97b22f2218a4a015a3efa82c2fbc8b9
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-02 11:53:47 +0530

    refactor added todoform

--- client/src/components/TodoForm.tsx
+++ client/src/components/TodoForm.tsx
@@ -5,62 +5,61 @@ import { IoMdAdd } from "react-icons/io";
 import { BASE_URL } from "../App";
 
 const TodoForm = () => {
-	const [newTodo, setNewTodo] = useState("");
+  const [newTodo, setNewTodo] = useState("");
 
-	const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
+  const { mutate: createTodo, isPending: isCreating } = useMutation({
+    mutationKey: ["createTodo"],
+    mutationFn: async (e: React.FormEvent) => {
+      e.preventDefault();
+      try {
+        const res = await fetch(BASE_URL + `/todos`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ body: newTodo }),
+        });
+        const data = await res.json();
 
-	const { mutate: createTodo, isPending: isCreating } = useMutation({
-		mutationKey: ["createTodo"],
-		mutationFn: async (e: React.FormEvent) => {
-			e.preventDefault();
-			try {
-				const res = await fetch(BASE_URL + `/todos`, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ body: newTodo }),
-				});
-				const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
 
-				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong");
-				}
+        setNewTodo("");
+        return data;
+      } catch (error: any) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (error: any) => {
+      alert(error.message);
+    },
+  });
 
-				setNewTodo("");
-				return data;
-			} catch (error: any) {
-				throw new Error(error);
-			}
-		},
-		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: ["todos"] });
-		},
-		onError: (error: any) => {
-			alert(error.message);
-		},
-	});
-
-	return (
-		<form onSubmit={createTodo}>
-			<Flex gap={2}>
-				<Input
-					type='text'
-					value={newTodo}
-					onChange={(e) => setNewTodo(e.target.value)}
-					ref={(input) => input && input.focus()}
-				/>
-				<Button
-					mx={2}
-					type='submit'
-					_active={{
-						transform: "scale(.97)",
-					}}
-				>
-					{isCreating ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
-				</Button>
-			</Flex>
-		</form>
-	);
+  return (
+    <form onSubmit={createTodo}>
+      <Flex gap={2}>
+        <Input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          ref={(input) => input && input.focus()}
+        />
+        <Button
+          mx={2}
+          type="submit"
+          _active={{
+            transform: "scale(.97)",
+          }}
+        >
+          {isCreating ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
+        </Button>
+      </Flex>
+    </form>
+  );
 };
 export default TodoForm;
