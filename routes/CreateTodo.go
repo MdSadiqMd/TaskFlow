0ba25e02800be94f4d149dbc87ad9a7a6af9a8a3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-02 13:34:08 +0530

    refactor createtodo oid generate

--- routes/CreateTodo.go
+++ routes/CreateTodo.go
@@ -6,21 +6,30 @@ import (
 	"github.com/MdSadiqMd/TaskFlow/models"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
 )
 
 func CreateTodo(c *fiber.Ctx) error {
 	todo := new(models.Todo)
 	if err := c.BodyParser(todo); err != nil {
-		return err
+		log.Println("Error parsing body:", err)
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid request body"})
 	}
 	if todo.Body == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "Empty Todo Cannot be created"})
+		log.Println("Empty Todo Body")
+		return c.Status(400).JSON(fiber.Map{"error": "Empty Todo cannot be created"})
 	}
 	insertResult, err := db.Collection.InsertOne(context.Background(), todo)
 	if err != nil {
-		return err
+		log.Println("Failed to insert todo:", err)
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to insert todo", "details": err.Error()})
 	}
-	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
-	
+	if oid, ok := insertResult.InsertedID.(primitive.ObjectID); ok {
+		todo.ID = oid
+	} else {
+		log.Println("Failed to cast InsertedID to ObjectID")
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to get inserted ID"})
+	}
+
 	return c.Status(201).JSON(todo)
 }
